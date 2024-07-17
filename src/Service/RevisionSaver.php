3f4cbbc3229ce00a5d5b7490b2443ffa996527e5
GergesShamon <gerges.shamoon.dev@gmail.com>
2024-07-17 13:52:37 +0300

    [RevisionSaver] Check for bugs and null pointers

--- src/Service/RevisionSaver.php
+++ src/Service/RevisionSaver.php
@@ -33,49 +33,49 @@ class RevisionSaver {
 	/**
 	 * @param EditInfo|null $editInfo
 	 *
-	 * @throws RuntimeException
-	 * @throws InvalidArgumentException
+	 * @throws RuntimeException if Content is not an EntityDocument
+	 * @throws InvalidArgumentException if $editInfo is null and the revision does not have an EditInfo
 	 * @return Item|Property new version of the entity
 	 */
 	public function save( Revision $revision, EditInfo $editInfo = null ): object {
-		if ( !$revision->getContent()->getData() instanceof EntityDocument ) {
-			throw new RuntimeException( 'Can only save Content of EntityDocuments' );
+		$content = $revision->getContent();
+		$data = $content->getData();
+		if (!$data instanceof EntityDocument) {
+			throw new RuntimeException('Can only save Content of EntityDocuments');
 		}
 
-		/** @var Item|Property $entity */
-		$entity = $revision->getContent()->getData();
-		$serialized = $this->entitySerializer->serialize( $entity );
+		$entity = $data;
+		$serialized = $this->entitySerializer->serialize($entity);
 
 		$params = [
-			'data' => json_encode( $serialized )
+			'data' => json_encode($serialized)
 		];
 
 		$revId = $revision->getId();
-		if ( $revId !== null ) {
+		if ($revId !== null) {
 			$params['baserevid'] = $revId;
 		}
 
 		$entityId = $entity->getId();
-		if ( $entityId !== null ) {
+		if ($entityId !== null) {
 			$params['id'] = $entityId->getSerialization();
 
 			// If we are provided an empty entity, then set the clear flag
-			if ( $entity->isEmpty() ) {
+			if ($entity->isEmpty()) {
 				$params['clear'] = true;
 			}
-
-
 		} else {
 			$params['new'] = $entity->getType();
 		}
 
 		// If no editInfo is explicitly passed call back to the one in the revision?
-		if ( $editInfo === null ) {
-			$editInfo = $revision->getEditInfo();
+		if ($editInfo === null && $revision->getEditInfo() === null) {
+			throw new InvalidArgumentException('No EditInfo provided and the revision does not have one');
 		}
 
-		$result = $this->api->postRequest( 'wbeditentity', $params, $editInfo );
-		return $this->entityDeserializer->deserialize( $result['entity'] );
+		$editInfo = $editInfo ?? $revision->getEditInfo();
+		$result = $this->api->postRequest('wbeditentity', $params, $editInfo);
+		return $this->entityDeserializer->deserialize($result['entity']);
 	}
 
 }
